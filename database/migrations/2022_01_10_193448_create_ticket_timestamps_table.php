834bc4222042d260942aef1cfe340b9ea288cf26
ckodhiambo <ckodhiambo1@gmail.com>
2022-01-12 00:27:58 +0300

    My latest GitHub push today - Date subtraction enabled.

---
+++ database/migrations/2022_01_10_193448_create_ticket_timestamps_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketTimestampsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket_timestamps', function (Blueprint $table) {
+            $table->id();
+            $table->integer('ticket_id');
+            $table->string('old_status');
+            $table->string('new_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket_timestamps');
+    }
+}
